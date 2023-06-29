480556f143a1ed1f12648f0ea0494fc84002486c
httpjamesm <github@httpjames.space>
2023-06-29 18:09:17 -0400

    fix: remove unnecessary useMemo dep

--- src/pages/posts/CommunitiesPage.tsx
+++ src/pages/posts/CommunitiesPage.tsx
@@ -104,7 +104,7 @@ export default function CommunitiesPage() {
     return communities.filter((c) =>
       favouriteCommunityHandles?.includes(getHandle(c))
     );
-  }, [favouriteCommunityHandles, communityByHandle, communities]);
+  }, [favouriteCommunityHandles, communities]);
 
   return (
     <IonPage ref={pageRef}>
