f0aeab86b01208d073936606e4f5f10d2830edde
Michael Förg <michael.foerg@pentlandfirth.com>
2023-07-04 18:54:38 +0200

    moved to index.css

--- src/theme/variables.ts
+++ src/theme/variables.ts
@@ -99,10 +99,6 @@ export const baseVariables = css`
     --ion-background-color: var(--ion-color-step-50, #f2f2f7);
     --ion-item-background: #fff;
   }
-
-  ion-icon {
-    -webkit-transform: translate3d(0, 0, 0);
-  }
 `;
 
 export const lightVariables = css`
