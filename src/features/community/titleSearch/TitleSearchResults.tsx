c8974e463c650f05d6f282c87fff7ab27cff2d6c
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2023-07-05 22:43:37 -0500

    Add graceful keyboard handling logic

--- src/features/community/titleSearch/TitleSearchResults.tsx
+++ src/features/community/titleSearch/TitleSearchResults.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useMemo, useState } from "react";
+import { useContext, useEffect, useMemo, useRef, useState } from "react";
 import { TitleSearchContext } from "./TitleSearchProvider";
 import styled from "@emotion/styled";
 import { useDebounce } from "usehooks-ts";
@@ -27,6 +27,10 @@ const Backdrop = styled.div`
   justify-content: center;
 `;
 
+const KeyboardContent = styled.div`
+  display: flex;
+`;
+
 const Contents = styled.div`
   --background: ${({ theme }) =>
     theme.dark ? "var(--ion-color-step-100)" : "var(--ion-background-color)"};
@@ -35,11 +39,13 @@ const Contents = styled.div`
   width: 100%;
   max-width: 500px;
   width: calc(100vw - 2rem);
-  min-height: 200px;
+  min-height: 175px;
   max-height: 450px;
   overflow: auto;
   margin: 1rem;
   border-radius: 0.5rem;
+
+  overscroll-behavior: contain;
 
   ion-item {
     --ion-item-background: ${({ theme }) =>
@@ -77,6 +83,10 @@ export default function TitleSearchResults() {
   const client = useClient();
   const follows = useAppSelector((state) => state.auth.site?.my_user?.follows);
   const buildGeneralBrowseLink = useBuildGeneralBrowseLink();
+  const [viewportHeight, setViewportHeight] = useState(
+    document.documentElement.clientHeight
+  );
+  const contentRef = useRef<HTMLDivElement>(null);
 
   const results: Result[] = useMemo(() => {
     return uniqBy(
@@ -108,6 +118,27 @@ export default function TitleSearchResults() {
     }
   });
 
+  useEffect(() => {
+    const onResize = () => {
+      // For the rare legacy browsers that don't support it
+      if (!window.visualViewport || !contentRef.current) {
+        return;
+      }
+
+      setViewportHeight(
+        window.visualViewport.height -
+          contentRef.current.getBoundingClientRect().top -
+          16
+      );
+    };
+
+    window.visualViewport?.addEventListener("resize", onResize);
+
+    return () => {
+      window.visualViewport?.removeEventListener("resize", onResize);
+    };
+  }, []);
+
   async function asyncSearch() {
     const result = await client.search({
       q: debouncedSearch,
@@ -124,33 +155,38 @@ export default function TitleSearchResults() {
 
   return (
     <Backdrop onClick={() => setSearching(false)}>
-      <Contents onClick={(e) => e.stopPropagation()}>
-        <IonList>
-          {results.map((c) => (
-            <IonItem
-              onClick={() => {
-                setSearching(false);
+      <KeyboardContent
+        ref={contentRef}
+        style={{ maxHeight: `${viewportHeight}px` }}
+      >
+        <Contents onClick={(e) => e.stopPropagation()}>
+          <IonList>
+            {results.map((c) => (
+              <IonItem
+                onClick={() => {
+                  setSearching(false);
 
-                let route;
+                  let route;
 
-                if ("type" in c) {
-                  route = buildGeneralBrowseLink(`/${c.type}`);
-                } else {
-                  route = buildGeneralBrowseLink(`/c/${getHandle(c)}`);
-                }
+                  if ("type" in c) {
+                    route = buildGeneralBrowseLink(`/${c.type}`);
+                  } else {
+                    route = buildGeneralBrowseLink(`/c/${getHandle(c)}`);
+                  }
 
-                router.push(route, undefined, undefined, undefined, () =>
-                  createAnimation()
-                );
-              }}
-              key={c.id}
-              routerDirection="none"
-            >
-              {"type" in c ? c.label : getHandle(c)}
-            </IonItem>
-          ))}
-        </IonList>
-      </Contents>
+                  router.push(route, undefined, undefined, undefined, () =>
+                    createAnimation()
+                  );
+                }}
+                key={c.id}
+                routerDirection="none"
+              >
+                {"type" in c ? c.label : getHandle(c)}
+              </IonItem>
+            ))}
+          </IonList>
+        </Contents>
+      </KeyboardContent>
     </Backdrop>
   );
 }
