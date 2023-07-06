1d5d0e798d09cbab5dac9ab3eef181027750ea12
robertnabbe <robertnabbe@gmail.com>
2023-07-06 18:58:58 -0500

    lint fix

--- src/features/post/inFeed/large/LargePost.tsx
+++ src/features/post/inFeed/large/LargePost.tsx
@@ -42,19 +42,13 @@ const Title = styled.div<{ isRead: boolean }>`
     `}
 `;
 
-const Details = styled.div<{ isRead: boolean }>`
+const Details = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
 
   font-size: 0.8em;
   color: var(--ion-color-medium);
-
-  ${({ isRead }) =>
-    isRead &&
-    css`
-      opacity: ${readOpacity};
-    `};
 `;
 
 const LeftDetails = styled.div`
