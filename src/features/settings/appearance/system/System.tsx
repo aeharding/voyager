21bc4cef0d346f7a2e1e7bdc1ef99084bb597d72
Mike Gregory <mike.ja.gregory@gmail.com>
2023-07-19 16:21:17 +0100

    Replace DarkMode and UserDarkMode with ThemeSelector in settings

--- src/features/settings/appearance/system/System.tsx
+++ src/features/settings/appearance/system/System.tsx
@@ -1,15 +1,10 @@
 import { IonLabel, IonList } from "@ionic/react";
-import DarkMode from "./DarkMode";
+
 import { ListHeader } from "../TextSize";
 import DeviceMode from "./DeviceMode";
-import { useAppSelector } from "../../../../store";
-import UserDarkMode from "./UserDarkMode";
+import ThemeSelector from "./ThemeSelector";
 
 export default function System() {
-  const { usingSystemDarkMode } = useAppSelector(
-    (state) => state.settings.appearance.dark
-  );
-
   return (
     <>
       <ListHeader>
@@ -17,10 +12,9 @@ export default function System() {
       </ListHeader>
       <IonList inset>
         <DeviceMode />
-        <DarkMode />
       </IonList>
 
-      {!usingSystemDarkMode && <UserDarkMode />}
+      <ThemeSelector />
     </>
   );
 }
