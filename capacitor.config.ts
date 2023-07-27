2431111fb27a255aa48731c7390aa5cec5c12ee8
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2023-07-26 19:08:14 -0500

    (chore) update app bundle id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "io.ionic.starter",
+  appId: "app.vger.voyager",
   appName: "Voyager",
   webDir: "dist",
   server: {
