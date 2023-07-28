f129611203f6410d336b610d1d03f3bb40ccc234
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2023-07-27 22:07:40 -0500

    Re-enable ion-content-scroll-host with empty workaround

--- src/pages/shared/FeedContent.tsx
+++ src/pages/shared/FeedContent.tsx
@@ -1,13 +1,12 @@
 import { IonContent } from "@ionic/react";
 import React from "react";
+import { isNative } from "../../helpers/device";
 
 interface FeedContentProps {
   children: React.ReactNode;
 }
 
-// There is some strange behavior that needs to be sorted before this
-// can be enabled
-export const isSafariFeedHackEnabled = true;
+export const isSafariFeedHackEnabled = !isNative();
 
 // All of this terrible code is to deal with safari being safari >:(
 // https://bugs.webkit.org/show_bug.cgi?id=222654
