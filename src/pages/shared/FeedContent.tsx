6811749fb5d773c81a38fe94109d41646bc49157
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2023-07-27 20:34:49 -0500

    Fix refresh empty feed not working on native iOS (#536)

--- src/pages/shared/FeedContent.tsx
+++ src/pages/shared/FeedContent.tsx
@@ -1,12 +1,13 @@
 import { IonContent } from "@ionic/react";
 import React from "react";
-import { isNative } from "../../helpers/device";
 
 interface FeedContentProps {
   children: React.ReactNode;
 }
 
-export const isSafariFeedHackEnabled = !isNative();
+// There is some strange behavior that needs to be sorted before this
+// can be enabled
+export const isSafariFeedHackEnabled = true;
 
 // All of this terrible code is to deal with safari being safari >:(
 // https://bugs.webkit.org/show_bug.cgi?id=222654
