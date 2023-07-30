c0498dd83add83adb0b3a81e09f180f5b9e6817d
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2023-07-30 14:21:25 -0500

    Fix share url not being thread link in certain circumstances (#561)
    Resolves #255

--- src/features/post/shared/MoreActions.tsx
+++ src/features/post/shared/MoreActions.tsx
@@ -266,7 +266,7 @@ export default function MoreActions({
               break;
             }
             case "share": {
-              navigator.share({ url: post.post.url ?? post.post.ap_id });
+              navigator.share({ url: post.post.ap_id });
 
               break;
             }
