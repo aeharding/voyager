7c1eab9efb74da368c9f4f562bc2908717bc82b5
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2023-07-30 11:05:41 -0500

    Revert "Add image thumbnails when possible (#552)" (#555)
    This reverts commit 9fd15e411351f17d43c5436b1c7df6402049b94d.

--- src/features/labels/img/ItemIcon.tsx
+++ src/features/labels/img/ItemIcon.tsx
@@ -1,7 +1,6 @@
 import styled from "@emotion/styled";
 import { Community, Person } from "lemmy-js-client";
 import FakeIcon from "../../shared/FakeIcon";
-import { getImageSrc } from "../../../services/lemmy";
 
 const SubImgIcon = styled.img<{ size: number }>`
   width: ${({ size }) => `${size}px`};
@@ -25,14 +24,7 @@ export default function ItemIcon({ item, size, className }: ItemIconProps) {
 
   const icon = "posting_restricted_to_mods" in item ? item.icon : item.avatar;
 
-  if (icon)
-    return (
-      <SubImgIcon
-        src={getImageSrc(icon, { size })}
-        size={size}
-        className={className}
-      />
-    );
+  if (icon) return <SubImgIcon src={icon} size={size} className={className} />;
 
   return (
     <FakeIcon
