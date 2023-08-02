cc207e9c7b23645c0f15b311c30eabca576f7b75
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2023-08-01 21:33:23 -0500

    Add text selection inside comment reply (#573)
    Note: This is buggy on Safari because Safari

--- src/features/comment/reply/ItemReplyingTo.tsx
+++ src/features/comment/reply/ItemReplyingTo.tsx
@@ -7,15 +7,16 @@ import Vote from "../../labels/Vote";
 import Ago from "../../labels/Ago";
 import CommentContent from "../CommentContent";
 import Edited from "../../labels/Edited";
+import { TouchEvent } from "react";
 
 const Container = styled.div`
   padding: 1rem;
   background: var(--ion-color-light);
-  pointer-events: none;
   font-size: 0.875em;
 
   a {
     color: inherit !important;
+    pointer-events: none;
   }
 `;
 
@@ -32,12 +33,24 @@ const StyledAgo = styled(Ago)`
   margin-left: auto;
 `;
 
+const CommentContentWrapper = styled.div`
+  user-select: text;
+`;
+
 interface ItemReplyingToProps {
   item: CommentView | PostView;
 }
 
 export default function ItemReplyingTo({ item }: ItemReplyingToProps) {
   const payload = "comment" in item ? item.comment : item.post;
+
+  function stopPropagationIfNeeded(e: TouchEvent) {
+    if (!window.getSelection()?.toString()) return true;
+
+    e.stopPropagation();
+
+    return true;
+  }
 
   return (
     <Container>
@@ -47,7 +60,9 @@ export default function ItemReplyingTo({ item }: ItemReplyingToProps) {
         <Edited item={item} />
         <StyledAgo date={payload.published} />
       </Header>
-      <CommentContent item={payload} />
+      <CommentContentWrapper onTouchMoveCapture={stopPropagationIfNeeded}>
+        <CommentContent item={payload} />
+      </CommentContentWrapper>
     </Container>
   );
 }
