8c80e322a0f33477deab913510f171bea7add886
Sharun <sharunksplus@gmail.com>
2023-10-13 19:49:59 -0400

    new hook: useFetchCommunity

--- src/pages/shared/CommunityPage.tsx
+++ src/pages/shared/CommunityPage.tsx
@@ -4,9 +4,8 @@ import { Redirect, useParams } from "react-router";
 import AppBackButton from "../../features/shared/AppBackButton";
 import PostSort from "../../features/feed/PostSort";
 import MoreActions from "../../features/community/MoreActions";
-import { useAppDispatch, useAppSelector } from "../../store";
-import { useCallback, useEffect } from "react";
-import { getCommunity } from "../../features/community/communitySlice";
+import { useAppSelector } from "../../store";
+import { useCallback } from "react";
 import { useBuildGeneralBrowseLink } from "../../helpers/routes";
 import useClient from "../../helpers/useClient";
 import { LIMIT } from "../../services/lemmy";
@@ -22,18 +21,14 @@ import { markReadOnScrollSelector } from "../../features/settings/settingsSlice"
 import FeedContent from "./FeedContent";
 import FeedContextProvider from "../../features/feed/FeedContext";
 import PostFabs from "../../features/feed/postFabs/PostFabs";
+import useFetchCommunity from "./useFetchCommunity";
 
 export default function CommunityPage() {
   const buildGeneralBrowseLink = useBuildGeneralBrowseLink();
-  const dispatch = useAppDispatch();
   const { community, actor } = useParams<{
     community: string;
     actor: string;
   }>();
-
-  const communityByHandle = useAppSelector(
-    (state) => state.community.communityByHandle,
-  );
 
   const client = useClient();
   const sort = useAppSelector((state) => state.post.sort);
@@ -55,13 +50,6 @@ export default function CommunityPage() {
     [client, community, sort, jwt],
   );
 
-  useEffect(() => {
-    if (communityByHandle[community]) return;
-
-    dispatch(getCommunity(community));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [community]);
-
   if (community.includes("@") && community.split("@")[1] === actor)
     return (
       <Redirect
@@ -72,7 +60,7 @@ export default function CommunityPage() {
 
   const feed = <PostCommentFeed fetchFn={fetchFn} communityName={community} />;
 
-  const communityView = communityByHandle[community];
+  const communityView = useFetchCommunity(community);
 
   return (
     <FeedContextProvider>
