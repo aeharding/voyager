8eb3202c52c2a86587413df72075a0a15c92b05c
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2023-10-26 12:28:18 -0500

    Add better feed video handling (#833)
    - Only play video if mostly on screen
    - Progress bar

--- src/features/shared/Video.tsx
+++ src/features/shared/Video.tsx
@@ -1,11 +1,40 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
-import { useIonViewWillEnter, useIonViewWillLeave } from "@ionic/react";
 import { Dictionary } from "@reduxjs/toolkit";
-import { useEffect, useRef } from "react";
+import { ChangeEvent, useCallback, useEffect, useRef, useState } from "react";
+import { useInView } from "react-intersection-observer";
+
+const Container = styled.div`
+  position: relative;
+  overflow: hidden;
+`;
+
+const Progress = styled.progress`
+  position: absolute;
+  bottom: -6px;
+  right: 0;
+  left: 0;
+  width: 100%;
+  appearance: none;
+  height: 12px;
+  transform: translate3d(0, 0, 0);
+
+  &::-webkit-progress-bar {
+    background: rgba(0, 0, 0, 0.0045);
+    backdrop-filter: blur(30px);
+  }
+
+  &::-webkit-progress-value {
+    background: rgba(255, 255, 255, 0.3);
+  }
+`;
 
 const VideoEl = styled.video<{ blur?: boolean }>`
   width: 100%;
+  max-height: calc(100vh - 60px);
+  object-fit: cover;
+
+  overflow: hidden;
 
   ${({ blur }) =>
     blur &&
@@ -29,52 +58,69 @@ interface VideoProps {
 const videoPlaybackPlace: Dictionary<number> = {};
 
 export default function Video({ src, controls, blur, className }: VideoProps) {
-  const videoRef = useRef<HTMLVideoElement | null>(null);
-
-  useIonViewWillLeave(() => {
-    savePlace();
+  const [inViewRef, inView] = useInView({
+    threshold: 0.5,
   });
+  const videoRef = useRef<HTMLVideoElement>();
+  const [progress, setProgress] = useState(0);
 
-  useIonViewWillEnter(() => {
-    resume();
-  });
+  const setRefs = useCallback(
+    (node: HTMLVideoElement) => {
+      // Ref's from useRef needs to have the node assigned to `current`
+      videoRef.current = node;
+      // Callback refs, like the one from `useInView`, is a function that takes the node as an argument
+      inViewRef(node);
+    },
+    [inViewRef],
+  );
 
-  useEffect(() => {
-    resume();
-
-    return () => {
-      savePlace();
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  function savePlace() {
+  const savePlace = useCallback(() => {
     if (!videoRef.current) return;
     if (blur) return;
 
     videoPlaybackPlace[src] = videoRef.current.currentTime;
     videoRef.current.pause();
-  }
+  }, [blur, src]);
 
-  function resume() {
+  const resume = useCallback(() => {
     if (!videoRef.current) return;
     if (blur) return;
 
     videoRef.current.currentTime = videoPlaybackPlace[src] ?? 0;
     videoRef.current.play();
-  }
+  }, [blur, src]);
 
-  return (
-    <VideoEl
-      className={className}
-      ref={videoRef}
-      src={src}
-      blur={blur}
-      loop
-      muted
-      playsInline
-      autoPlay={!blur}
-      controls={controls}
-    />
+  useEffect(() => {
+    if (!videoRef || !videoRef.current) {
+      return;
+    }
+
+    if (inView) {
+      resume();
+    } else {
+      savePlace();
+    }
+  }, [inView, savePlace, resume]);
+
+  const videoEl = (
+    <Container>
+      <VideoEl
+        className={className}
+        ref={setRefs}
+        src={`${src}#t=0.001`}
+        blur={blur}
+        loop
+        muted
+        playsInline
+        autoPlay={false}
+        controls={controls}
+        onTimeUpdate={(e: ChangeEvent<HTMLVideoElement>) => {
+          setProgress(e.target.currentTime / e.target.duration);
+        }}
+      />
+      <Progress value={progress} />
+    </Container>
   );
+
+  return videoEl;
 }
