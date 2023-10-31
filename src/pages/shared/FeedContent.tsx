f51d0f720ff0ee33a0e5a92825e96e5e8eb3bd8d
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2023-10-31 12:48:10 -0500

    Fix pull to refresh glitches (#874)
    Resolves #873
    Resolves #718

--- src/pages/shared/FeedContent.tsx
+++ src/pages/shared/FeedContent.tsx
@@ -1,6 +1,10 @@
 import { IonContent } from "@ionic/react";
 import React from "react";
-import { isAndroid, isNative } from "../../helpers/device";
+import {
+  isAppleDeviceInstalledToHomescreen,
+  isNative,
+  isTouchDevice,
+} from "../../helpers/device";
 
 interface FeedContentProps {
   children: React.ReactNode;
@@ -9,17 +13,15 @@ interface FeedContentProps {
 
 // There is some strange behavior that needs to be sorted before this
 // can be enabled
-export const isSafariFeedHackEnabled = !isNative() || isAndroid();
+export const isSafariFeedHackEnabled =
+  !isNative() && isAppleDeviceInstalledToHomescreen() && isTouchDevice();
 
 // All of this terrible code is to deal with safari being safari >:(
 // https://bugs.webkit.org/show_bug.cgi?id=222654
 
 export default function FeedContent({ children, className }: FeedContentProps) {
-  if (isSafariFeedHackEnabled)
-    return <IonContent className={className}>{children}</IonContent>;
-
   return (
-    <IonContent scrollY={false} fullscreen className={className}>
+    <IonContent className={className} scrollY={isSafariFeedHackEnabled}>
       {children}
     </IonContent>
   );
