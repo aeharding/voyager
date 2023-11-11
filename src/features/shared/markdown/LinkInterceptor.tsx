3fb4718f9054a0c0db22a69056b0ef65c926df8a
Dukk <acedaksh07@gmail.com>
2023-11-11 13:33:55 -0500

    fix: open spoilers without collapsing comments

--- src/features/shared/markdown/LinkInterceptor.tsx
+++ src/features/shared/markdown/LinkInterceptor.tsx
@@ -43,7 +43,7 @@ function LinkInterceptorUnstyled({
   // Sometimes markdown thinks things are URLs that aren't URLs
   if (!absoluteHref) return props.children;
   if (absoluteHref == "https://spoiler/") return (
-    <details>
+    <details onClick={(e) => e.stopPropagation()}>
       <summary>{props.title ? props.title : "spoiler"}</summary>
       {props.children}
     </details>
