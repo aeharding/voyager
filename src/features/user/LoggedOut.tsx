32de1beb347cb7c08f1a69bfaa9010eaae843c7e
Sharun <sharunksplus@gmail.com>
2023-11-20 18:03:17 -0500

    ux: instance selector would have the currently selected instance in the top of the list

--- src/features/user/LoggedOut.tsx
+++ src/features/user/LoggedOut.tsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { updateConnectedInstance } from "../auth/authSlice";
 import { swapHorizontalOutline } from "ionicons/icons";
 import { getCustomServers } from "../../services/app";
+import { without } from "lodash";
 
 const Incognito = styled(IncognitoSvg)`
   opacity: 0.1;
@@ -20,10 +21,15 @@ const Incognito = styled(IncognitoSvg)`
 
 export default function LoggedOut() {
   const dispatch = useAppDispatch();
-  const connectedInstance = useAppSelector(
-    (state) => state.auth.connectedInstance,
-  );
+  const { connectedInstance } = useAppSelector((state) => state.auth);
   const [pickerOpen, setPickerOpen] = useState(false);
+
+  // push the currently connected instance to
+  // the top of the list in the picker
+  const customServers = [
+    connectedInstance,
+    ...without(getCustomServers(), connectedInstance),
+  ];
 
   return (
     <>
@@ -59,7 +65,7 @@ export default function LoggedOut() {
         columns={[
           {
             name: "server",
-            options: getCustomServers().map((server) => ({
+            options: customServers.map((server) => ({
               text: server,
               value: server,
             })),
