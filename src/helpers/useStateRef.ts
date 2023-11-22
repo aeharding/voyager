be21791b24d5d36f6dac410ec94f7c800b6685fc
Avinash Thakur <19588421+80avin@users.noreply.github.com>
2023-11-22 06:27:06 +0530

    Fix delete/cancel staying on screen after post/comment submission (#956)
    - make setCanDismiss synchronous
    - remove setTimeouts
    
    Resolves #621
    
    Co-authored-by: Alexander Harding <2166114+aeharding@users.noreply.github.com>

---
+++ src/helpers/useStateRef.ts
@@ -0,0 +1,22 @@
+import { useCallback, useRef, useState } from "react";
+
+export default function useStateRef<T>(defaultValue: T) {
+  const [stateValue, setStateValue] = useState(defaultValue);
+  const stateRef = useRef(stateValue);
+
+  const setState: typeof setStateValue = useCallback((newValueOrSetter) => {
+    if (newValueOrSetter instanceof Function) {
+      // using `instanceof` to avoid https://github.com/microsoft/TypeScript/issues/46066
+      setStateValue((prevState) => {
+        const _newValue = newValueOrSetter(prevState);
+        stateRef.current = _newValue;
+        return _newValue;
+      });
+    } else {
+      stateRef.current = newValueOrSetter;
+      setStateValue(newValueOrSetter);
+    }
+  }, []);
+
+  return [stateRef, setState] as const;
+}
