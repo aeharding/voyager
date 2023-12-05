937c57820b57591b931eb5b2e2e2de8df3167009
Sharun <sharunksplus@gmail.com>
2023-12-04 20:49:53 -0500

    lint fix

--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -61,7 +61,9 @@ export default function Login({
   const presentToast = useAppToast();
   const dispatch = useAppDispatch();
   const [servers] = useState(getCustomServers());
-  const connectedInstance = useAppSelector((state) => state.auth.connectedInstance);
+  const connectedInstance = useAppSelector(
+    (state) => state.auth.connectedInstance,
+  );
   const [server, setServer] = useState(connectedInstance);
   const [customServer, setCustomServer] = useState("");
   const [serverConfirmed, setServerConfirmed] = useState(false);
