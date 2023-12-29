ffc892c356106c4b14a413539c0d881a0f3ba737
CmdrKeen <0xCmdrKeen@pm.me>
2023-12-28 17:23:38 -0800

    Add instance name to message

--- src/features/moderation/logs/types/banFromInstance.ts
+++ src/features/moderation/logs/types/banFromInstance.ts
@@ -1,6 +1,6 @@
 import { ModBanView } from "lemmy-js-client";
 import { LogEntryData } from "../ModlogItem";
-import { getHandle } from "../../../../helpers/lemmy";
+import { getHandle, getItemActorName } from "../../../../helpers/lemmy";
 import { buildBaseData } from "./shared";
 import { buildUserLink } from "../../../../helpers/appLinkBuilder";
 import { ban, refresh } from "ionicons/icons";
@@ -10,7 +10,9 @@ export default function banFromInstance(item: ModBanView): LogEntryData {
     icon: item.mod_ban.banned ? ban : refresh,
     title: `${item.mod_ban.banned ? "Banned" : "Unbanned"} User`,
     by: item.moderator ? getHandle(item.moderator) : undefined,
-    message: getHandle(item.banned_person),
+    message:
+      getHandle(item.banned_person) +
+      (item.moderator ? ` from ${getItemActorName(item.moderator)}` : ""),
     link: buildUserLink(item.banned_person),
     ...buildBaseData(item.mod_ban),
   };
