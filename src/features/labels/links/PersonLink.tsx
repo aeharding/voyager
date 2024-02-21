8513a5c1ba5d5f95a46697582304ac8bd3fea58e
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2024-02-20 20:59:57 -0600

    Add long press to block user (#1251)
    Resolves #1250

--- src/features/labels/links/PersonLink.tsx
+++ src/features/labels/links/PersonLink.tsx
@@ -2,15 +2,24 @@ import { getHandle } from "../../../helpers/lemmy";
 import { useBuildGeneralBrowseLink } from "../../../helpers/routes";
 import { Person } from "lemmy-js-client";
 import Handle from "../Handle";
-import { useAppSelector } from "../../../store";
+import store, { useAppDispatch, useAppSelector } from "../../../store";
 import { OInstanceUrlDisplayMode } from "../../../services/db";
 import AgeBadge from "./AgeBadge";
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 import { ShareImageContext } from "../../share/asImage/ShareAsImage";
 import { preventOnClickNavigationBug } from "../../../helpers/ionic";
 import { styled } from "@linaria/react";
 import { StyledLink, hideCss } from "./shared";
 import { cx } from "@linaria/core";
+import { LongPressOptions, useLongPress } from "use-long-press";
+import { useIonActionSheet } from "@ionic/react";
+import { removeCircleOutline } from "ionicons/icons";
+import { blockUser } from "../../user/userSlice";
+import useAppToast from "../../../helpers/useAppToast";
+import {
+  buildBlocked,
+  problemBlockingUser,
+} from "../../../helpers/toastMessages";
 
 const Prefix = styled.span`
   font-weight: normal;
@@ -36,18 +45,62 @@ export default function PersonLink({
   prefix,
   showBadge = true,
 }: PersonLinkProps) {
+  const presentToast = useAppToast();
+  const [presentActionSheet] = useIonActionSheet();
+  const dispatch = useAppDispatch();
   const buildGeneralBrowseLink = useBuildGeneralBrowseLink();
   const isAdmin = useAppSelector((state) => state.site.response?.admins)?.some(
     (admin) => admin.person.actor_id === person.actor_id,
   );
   const { hideUsernames } = useContext(ShareImageContext);
 
-  let color: string | undefined;
+  const onCommunityLinkLongPress = useCallback(() => {
+    const state = store.getState();
+
+    const blocks = state.site.response?.my_user?.person_blocks;
+    const isBlocked = blocks?.some(
+      (b) => getHandle(b.target) === getHandle(person),
+    );
+
+    presentActionSheet({
+      cssClass: "left-align-buttons",
+      buttons: [
+        {
+          text: `${isBlocked ? "Unblock" : "Block"} User`,
+          icon: removeCircleOutline,
+          role: "destructive",
+          handler: () => {
+            (async () => {
+              try {
+                await dispatch(blockUser(!isBlocked, person.id));
+              } catch (error) {
+                presentToast(problemBlockingUser);
+                throw error;
+              }
+
+              presentToast(buildBlocked(!isBlocked, getHandle(person)));
+            })();
+          },
+        },
+        {
+          text: "Cancel",
+          role: "cancel",
+        },
+      ],
+    });
+  }, [presentActionSheet, presentToast, dispatch, person]);
+
+  const bind = useLongPress(onCommunityLinkLongPress, {
+    cancelOnMovement: true,
+    onStart,
+  });
 
   const forceInstanceUrl =
     useAppSelector(
       (state) => state.settings.appearance.general.userInstanceUrlDisplay,
     ) === OInstanceUrlDisplayMode.WhenRemote;
+
+  let color: string | undefined;
 
   if (isAdmin) color = "var(--ion-color-danger)";
   else if (distinguished) color = "var(--ion-color-success)";
@@ -60,6 +113,7 @@ export default function PersonLink({
         e.stopPropagation();
         preventOnClickNavigationBug(e);
       }}
+      {...bind()}
       className={cx(className, hideUsernames ? hideCss : undefined)}
       style={{ color }}
     >
@@ -81,3 +135,7 @@ export default function PersonLink({
     </StyledLink>
   );
 }
+
+const onStart: LongPressOptions["onStart"] = (e) => {
+  e.stopPropagation();
+};
