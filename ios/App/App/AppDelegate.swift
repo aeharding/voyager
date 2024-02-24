513d43e7f0f93acb4ae3fb2ced4c1a96b459cef2
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2024-02-24 13:52:18 -0600

    oops

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -8,7 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, WCSessionDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.d
+        // Override point for customization after application launch.
 
         if WCSession.isSupported() {
             let session = WCSession.default
