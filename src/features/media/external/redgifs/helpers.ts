fd3f3fb35656d8c88827f32be18e518e343c7eca
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2024-03-24 20:06:13 -0500

    Cleanup

--- src/features/media/external/redgifs/helpers.ts
+++ src/features/media/external/redgifs/helpers.ts
@@ -1,5 +1,22 @@
-export const redgifUrlRegex = /^https:\/\/redgifs.com\/watch\/([a-z]+)/;
+import { isNative } from "../../../../helpers/device";
+
+export const redgifUrlRegex =
+  /^https:\/\/(?:www\.)?redgifs.com\/watch\/([a-z]+)/;
+
+/**
+ * Uncomment `return true` in dev to test with following command
+ *
+ * ```
+ * /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --disable-site-isolation-trials --disable-web-security --user-data-dir="~/tmp"
+ * ```
+ */
+export function platformSupportsRedgif() {
+  // return true;
+  return isNative();
+}
 
 export function isRedgif(url: string): boolean {
+  if (!platformSupportsRedgif()) return false;
+
   return redgifUrlRegex.test(url);
 }
--- src/features/settings/appearance/posts/EmbedExternalMedia.tsx
+++ src/features/settings/appearance/posts/EmbedExternalMedia.tsx
@@ -3,12 +3,15 @@ import { IonItem, IonToggle } from "@ionic/react";
 import { useAppDispatch, useAppSelector } from "../../../../store";
 import { setEmbedExternalMedia } from "../../settingsSlice";
 import { resetRedgifs } from "../../../media/external/redgifs/redgifsSlice";
+import { platformSupportsRedgif } from "../../../media/external/redgifs/helpers";
 
 export default function EmbedExternalMedia() {
   const dispatch = useAppDispatch();
   const embedExternalMedia = useAppSelector(
     (state) => state.settings.appearance.posts.embedExternalMedia,
   );
+
+  if (!platformSupportsRedgif()) return;
 
   return (
     <IonItem>
