1726f5b7890eec0781000c8f1fa4f9bd458cb83c
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2024-03-27 19:22:55 -0500

    More diff

--- src/core/globalCssOverrides.ts
+++ src/core/globalCssOverrides.ts
@@ -1,5 +1,7 @@
 import { css } from "@linaria/core";
 
+import "./syntaxHighlightCss";
+
 export default css`
   :global() {
     :root {
@@ -322,80 +324,6 @@ export default css`
       > *:last-child > *:last-child > *:last-child {
         margin-bottom: 0;
       }
-    }
-
-    .hljs-doctag,
-    .hljs-keyword,
-    .hljs-meta .hljs-keyword,
-    .hljs-template-tag,
-    .hljs-template-variable,
-    .hljs-type,
-    .hljs-variable.language_ {
-      color: #ff7b72;
-    }
-    .hljs-title,
-    .hljs-title.class_,
-    .hljs-title.class_.inherited__,
-    .hljs-title.function_ {
-      color: #d2a8ff;
-    }
-    .hljs-attr,
-    .hljs-attribute,
-    .hljs-literal,
-    .hljs-meta,
-    .hljs-number,
-    .hljs-operator,
-    .hljs-variable,
-    .hljs-selector-attr,
-    .hljs-selector-class,
-    .hljs-selector-id {
-      color: #79c0ff;
-    }
-    .hljs-regexp,
-    .hljs-string,
-    .hljs-meta .hljs-string {
-      color: #a5d6ff;
-    }
-    .hljs-built_in,
-    .hljs-symbol {
-      color: #ffa657;
-    }
-    .hljs-comment,
-    .hljs-code,
-    .hljs-formula {
-      color: #8b949e;
-    }
-    .hljs-name,
-    .hljs-quote,
-    .hljs-selector-tag,
-    .hljs-selector-pseudo {
-      color: #7ee787;
-    }
-    .hljs-subst {
-      color: #c9d1d9;
-    }
-    .hljs-section {
-      color: #1f6feb;
-      font-weight: bold;
-    }
-    .hljs-bullet {
-      color: #f2cc60;
-    }
-    .hljs-emphasis {
-      color: #c9d1d9;
-      font-style: italic;
-    }
-    .hljs-strong {
-      color: #c9d1d9;
-      font-weight: bold;
-    }
-    .hljs-addition {
-      color: #aff5b4;
-      background-color: #033a16;
-    }
-    .hljs-deletion {
-      color: #ffdcd7;
-      background-color: #67060c;
     }
   }
 `;
