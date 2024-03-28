25d75689953f5c697aded33ed68267fb92c3b724
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2024-03-28 17:11:00 -0500

    Fix compact post mark as read when opening image

--- src/features/post/inFeed/compact/CompactFeedPostMedia.tsx
+++ src/features/post/inFeed/compact/CompactFeedPostMedia.tsx
@@ -2,11 +2,10 @@ import { ComponentProps, useMemo } from "react";
 import Media, { getPostMedia } from "../../../media/gallery/Media";
 import { PostView } from "lemmy-js-client";
 
-export default function CompactFeedPostMedia({
-  post,
-  ...rest
-}: Omit<ComponentProps<typeof Media>, "src"> & { post: PostView }) {
-  const src = useMemo(() => getPostMedia(post), [post]);
+export default function CompactFeedPostMedia(
+  props: Omit<ComponentProps<typeof Media>, "src"> & { post: PostView },
+) {
+  const src = useMemo(() => getPostMedia(props.post), [props.post]);
 
-  if (src) return <Media src={src} {...rest} />;
+  if (src) return <Media {...props} src={src} />;
 }
