dfbe1f7d0fe5616224fee0df90599613c2074d3c
Aashu Anshuman <aashu.anshuman@gmail.com>
2024-04-05 14:53:08 +0100

    Setup Playwright for end-to-end testing (#1392)
    * Install Playwright
    
    * Add Playwright config, update Vite config
    
    * Vite tries to run all test files by default. The directory containing
      Playwright tests (`e2e/`) and `node_modules/` need to be excluded in
      vite.config.ts.
    
    * Add some basic tests for community feed page
    
    * Tests currently depend on locally stored test data.
    
    * Add GitHub action to run Playwright tests
    
    * Add e2e.yml to run Playwright tests in a container using an image
      provided by the Playwright team.
    * Update Playwright config to run tests against a production build of
      the app instead of the dev server.
    * Add script to package.json to run Playwright tests.
    
    * Remove trailing whitespace to pass Prettier check
    
    * Update e2e/testdata/posts.ts

---
+++ e2e/community-feed.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from "@playwright/test";
+
+import { posts } from "./testdata/posts";
+
+test("load community posts", async ({ page }) => {
+  await page.route("*/**/api/v3/post/list**", async (route) => {
+    await route.fulfill({ json: { posts } });
+  });
+
+  await page.goto("/");
+  await page.waitForURL("/posts/lemmy.world/all");
+
+  await expect(page).toHaveTitle("Voyager for Lemmy");
+
+  await expect(page.getByText(posts[0].post.name)).toBeVisible();
+});
+
+test("navigate to post on click", async ({ page }) => {
+  await page.route("*/**/api/v3/post/list**", async (route) => {
+    await route.fulfill({ json: { posts } });
+  });
+
+  await page.goto("/");
+
+  await page.getByText(posts[0].post.name).click();
+
+  await expect(page).toHaveURL(
+    "/posts/lemmy.world/c/community_1@test.lemmy/comments/999",
+  );
+});
---
+++ playwright.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./e2e",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "list",
+  use: {
+    baseURL: "http://localhost:4173",
+
+    trace: "on-first-retry",
+  },
+
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+    {
+      name: "Mobile Chrome",
+      use: { ...devices["Pixel 7"] },
+    },
+    {
+      name: "Mobile Safari",
+      use: { ...devices["iPhone 14"] },
+    },
+  ],
+
+  webServer: {
+    command: "pnpm preview",
+    url: "http://localhost:4173",
+    reuseExistingServer: !process.env.CI,
+  },
+});
