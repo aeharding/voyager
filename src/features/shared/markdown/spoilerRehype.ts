d436c62ff97ced9656c79d81ea81caa5c4cd22e5
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2024-07-09 21:37:48 -0500

    Fix spoiler/sub/super markdown text in post preview

--- src/features/shared/markdown/spoilerRehype.ts
+++ src/features/shared/markdown/spoilerRehype.ts
@@ -1,7 +1,7 @@
 /// <reference types="mdast-util-lemmy-spoiler" />
 
 import { visit } from "unist-util-visit";
-import { Root } from "mdast";
+import { Parent, Root } from "mdast";
 
 /**
  * Transform spoiler tree node to details/summary rehype html nodes
@@ -30,6 +30,31 @@ export default function spoilerRehype() {
             ],
           },
           ...node.children,
+        ];
+      }
+    });
+  };
+}
+
+/**
+ * Transform spoiler tree node to details/summary rehype html nodes (SUMMARY ONLY)
+ *
+ * TODO this may not be necessary if the logic can be moved to mdast-util-lemmy-spoiler
+ * https://github.com/orgs/micromark/discussions/163
+ */
+export function spoilerSummaryOnlyRehype() {
+  return function (tree: Root) {
+    visit(tree, function (node) {
+      if (node.type === "spoiler") {
+        // Transform the spoiler summary text to a paragraph node (for inline markdown component)
+        const nodeAsParagraph = node as Parent;
+        nodeAsParagraph.type = "paragraph";
+
+        nodeAsParagraph.children = [
+          {
+            type: "text",
+            value: node.name,
+          },
         ];
       }
     });
