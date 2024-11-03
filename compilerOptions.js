af638c40b297cdbc7ff5fb0bc44e4a4a36a58928
Alexander Harding <2166114+aeharding@users.noreply.github.com>
2024-11-03 00:17:04 -0500

    meh

--- compilerOptions.js
+++ compilerOptions.js
@@ -2,5 +2,5 @@ export default {
   environment: {
     enableTreatRefLikeIdentifiersAsRefs: true,
   },
-  // __unstable_donotuse_reportAllBailouts: true,
+  __unstable_donotuse_reportAllBailouts: !!process.env.RCALLBAILS,
 };
