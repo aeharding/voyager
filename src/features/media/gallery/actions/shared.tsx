35170d64c77277b3bfb8a0d4a7b35187b125c6b4
Alexander Harding <noreply@harding.dev>
2024-11-22 23:14:42 -0600

    type issues

--- src/features/media/gallery/actions/shared.tsx
+++ src/features/media/gallery/actions/shared.tsx
@@ -1,21 +1,24 @@
+import React from "react";
+
 import { cx } from "#/helpers/css";
 
 import styles from "./shared.module.css";
 
-export function BottomContainer() {
-  return <div className={styles.bottomContainer} />;
+export function BottomContainer({ children }: React.PropsWithChildren) {
+  return <div className={styles.bottomContainer}>{children}</div>;
 }
 
-interface BottomContainerActionsProps {
+interface BottomContainerActionsProps extends React.PropsWithChildren {
   withBg: boolean;
 }
 
 export function BottomContainerActions({
   withBg,
+  children,
 }: BottomContainerActionsProps) {
   return (
-    <div
-      className={cx(styles.bottomContainerActions, withBg && styles.withBg)}
-    />
+    <div className={cx(styles.bottomContainerActions, withBg && styles.withBg)}>
+      {children}
+    </div>
   );
 }
