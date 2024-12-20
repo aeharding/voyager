4628ddddb7557effe2821b9f63d348b5b4ff50bb
Alexander Harding <noreply@harding.dev>
2024-12-20 00:34:26 -0600

    Progress

--- ios/App/CapApp-SPM/Package.swift
+++ ios/App/CapApp-SPM/Package.swift
@@ -21,15 +21,15 @@ let package = Package(
         .package(name: "CapacitorShare", path: "../../../node_modules/.pnpm/@capacitor+share@6.0.3_@capacitor+core@6.2.0/node_modules/@capacitor/share"),
         .package(name: "CapacitorSplashScreen", path: "../../../node_modules/.pnpm/@capacitor+splash-screen@6.0.3_@capacitor+core@6.2.0/node_modules/@capacitor/splash-screen"),
         .package(name: "CapacitorStatusBar", path: "../../../node_modules/.pnpm/@capacitor+status-bar@6.0.2_@capacitor+core@6.2.0/node_modules/@capacitor/status-bar"),
-        .package(name: "CapacitorAndroidNavMode", path: "../../../../capacitor-android-nav-mode"),
-        .package(name: "CapacitorApplicationContext", path: "../../../../capacitor-application-context"),
-        .package(name: "CapacitorBiometricLock", path: "../../../../capacitor-biometric-lock"),
-        .package(name: "CapacitorClearCache", path: "../../../../capacitor-clear-cache"),
-        .package(name: "CapacitorLaunchNative", path: "../../../../capacitor-launch-native"),
-        .package(name: "CapacitorPluginSafeArea", path: "../../../../capacitor-plugin-safe-area"),
-        .package(name: "CapacitorReader", path: "../../../../capacitor-reader"),
-        .package(name: "CapacitorStashMedia", path: "../../../../capacitor-stash-media"),
-        .package(name: "CapacitorTips", path: "../../../../capacitor-tips")
+        .package(name: "CapacitorAndroidNavMode", path: "../../../node_modules/.pnpm/capacitor-android-nav-mode@1.1.1_@capacitor+core@6.2.0/node_modules/capacitor-android-nav-mode"),
+        .package(name: "CapacitorApplicationContext", path: "../../../node_modules/.pnpm/capacitor-application-context@1.1.1_@capacitor+core@6.2.0/node_modules/capacitor-application-context"),
+        .package(name: "CapacitorBiometricLock", path: "../../../node_modules/.pnpm/capacitor-biometric-lock@1.1.1_@capacitor+core@6.2.0/node_modules/capacitor-biometric-lock"),
+        .package(name: "CapacitorClearCache", path: "../../../node_modules/.pnpm/capacitor-clear-cache@1.1.1_@capacitor+core@6.2.0/node_modules/capacitor-clear-cache"),
+        .package(name: "CapacitorLaunchNative", path: "../../../node_modules/.pnpm/capacitor-launch-native@1.1.1_@capacitor+core@6.2.0/node_modules/capacitor-launch-native"),
+        .package(name: "CapacitorPluginSafeArea", path: "../../../node_modules/.pnpm/capacitor-plugin-safe-area@3.0.3_@capacitor+core@6.2.0/node_modules/capacitor-plugin-safe-area"),
+        .package(name: "CapacitorReader", path: "../../../node_modules/.pnpm/capacitor-reader@0.3.0_@capacitor+core@6.2.0/node_modules/capacitor-reader"),
+        .package(name: "CapacitorStashMedia", path: "../../../node_modules/.pnpm/capacitor-stash-media@2.1.1_@capacitor+core@6.2.0/node_modules/capacitor-stash-media"),
+        .package(name: "CapacitorTips", path: "../../../node_modules/.pnpm/capacitor-tips@1.1.1_@capacitor+core@6.2.0/node_modules/capacitor-tips")
     ],
     targets: [
         .target(
