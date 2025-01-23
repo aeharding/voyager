131776d0b46841277d5e28f419cb06df0fb9cf43
Alexander Harding <noreply@harding.dev>
2025-01-22 21:28:10 -0600

    fix: clarify read posts will always be hidden (#1822)

--- src/features/feed/useResetHiddenPosts.ts
+++ src/features/feed/useResetHiddenPosts.ts
@@ -1,14 +1,20 @@
 import { useIonActionSheet } from "@ionic/react";
 
 import { clearHidden } from "#/features/post/postSlice";
-import { useAppDispatch } from "#/store";
+import { useAppDispatch, useAppSelector } from "#/store";
 
 export default function useResetHiddenPosts() {
   const dispatch = useAppDispatch();
   const [presentActionSheet] = useIonActionSheet();
+  const neverShowReadPosts = useAppSelector(
+    (state) => state.settings.general.posts.neverShowReadPosts,
+  );
 
   return function (cb?: () => void) {
     presentActionSheet({
+      header: neverShowReadPosts
+        ? 'Note: You have "Never Show Previously Read" turned on, so read posts will remain hidden'
+        : undefined,
       buttons: [
         {
           text: "Reset hidden posts",
