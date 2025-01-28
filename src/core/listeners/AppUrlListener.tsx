f5c5857bd0bc6893c4eb8f37e88ade0ab9f9965b
Alexander Harding <noreply@harding.dev>
2025-01-28 17:51:17 -0600

    fix: deeplink can be flakey
    this depends on app restore state from background

--- src/core/listeners/AppUrlListener.tsx
+++ src/core/listeners/AppUrlListener.tsx
@@ -11,7 +11,14 @@ import { deepLinkFailed } from "#/helpers/toastMessages";
 import useAppToast from "#/helpers/useAppToast";
 import { useAppSelector } from "#/store";
 
+const PENDING_APP_URL_STORAGE_KEY = "app-url";
+
+App.addListener("appUrlOpen", (event) => {
+  localStorage.setItem(PENDING_APP_URL_STORAGE_KEY, event.url);
+});
+
 export default function AppUrlListener() {
+  const presentToast = useAppToast();
   const { redirectToLemmyObjectIfNeeded } = useLemmyUrlHandler();
   const knownInstances = useAppSelector(
     (state) => state.instances.knownInstances,
@@ -21,8 +28,7 @@ export default function AppUrlListener() {
   );
   const deepLinkReady = useAppSelector((state) => state.deepLinkReady.ready);
 
-  const appUrlToOpen = useRef<string | undefined>();
-  const presentToast = useAppToast();
+  const pendingResolveRef = useRef(false);
 
   const notReady =
     !knownInstances ||
@@ -30,24 +36,38 @@ export default function AppUrlListener() {
     !connectedInstance ||
     !deepLinkReady;
 
-  const onAppUrl = async (url: string) => {
-    if (notReady) {
-      appUrlToOpen.current = url;
-      return;
-    }
-
+  const onAppUrlIfNeeded = async (potentialUrl?: string) => {
     // wait for router to get into a good state before pushing
     // (needed for pushing user profiles from app startup)
-    const result = await redirectToLemmyObjectIfNeeded(url);
+    if (notReady) return;
+    if (pendingResolveRef.current) return;
+
+    const url =
+      localStorage.getItem(PENDING_APP_URL_STORAGE_KEY) || potentialUrl;
+
+    if (!url) return;
+
+    localStorage.removeItem(PENDING_APP_URL_STORAGE_KEY);
+
+    pendingResolveRef.current = true;
+
+    let result;
+
+    try {
+      result = await redirectToLemmyObjectIfNeeded(normalizeObjectUrl(url));
+    } finally {
+      pendingResolveRef.current = false;
+      localStorage.removeItem(PENDING_APP_URL_STORAGE_KEY);
+    }
 
     if (result === "not-found") presentToast(deepLinkFailed);
   };
 
-  const onAppUrlEvent = useEffectEvent(onAppUrl);
+  const onAppUrlIfNeededEvent = useEffectEvent(onAppUrlIfNeeded);
 
   useEffect(() => {
     const listener = App.addListener("appUrlOpen", (event) => {
-      onAppUrlEvent(normalizeObjectUrl(event.url));
+      onAppUrlIfNeededEvent(event.url);
     });
 
     return () => {
@@ -57,10 +77,8 @@ export default function AppUrlListener() {
 
   useEffect(() => {
     if (notReady) return;
-    if (!appUrlToOpen.current) return;
 
-    onAppUrlEvent(appUrlToOpen.current);
-    appUrlToOpen.current = undefined;
+    onAppUrlIfNeededEvent();
   }, [notReady]);
 
   return null;
