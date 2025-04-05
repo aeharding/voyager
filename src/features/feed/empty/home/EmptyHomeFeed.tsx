0404d63914e0de644e00d93290197cbd128e6dc4
Alexander Harding <noreply@harding.dev>
2025-04-05 18:07:06 -0500

    feat: starter packs (#1939)

---
+++ src/features/feed/empty/home/EmptyHomeFeed.tsx
@@ -0,0 +1,71 @@
+import { IonIcon, useIonModal } from "@ionic/react";
+import { IonItem } from "@ionic/react";
+import { IonList } from "@ionic/react";
+import { IonText } from "@ionic/react";
+import { duplicateOutline, earthOutline } from "ionicons/icons";
+import { use } from "react";
+
+import { PageContext } from "#/features/auth/PageContext";
+import { MaxWidthContainer } from "#/features/shared/AppContent";
+import { useBuildGeneralBrowseLink } from "#/helpers/routes";
+import { useOptimizedIonRouter } from "#/helpers/useOptimizedIonRouter";
+
+import StarterPacksModal from "./StarterPacksModal";
+
+export default function EmptyHomeFeed() {
+  const buildGeneralBrowseLink = useBuildGeneralBrowseLink();
+  const router = useOptimizedIonRouter();
+
+  const { pageRef } = use(PageContext);
+  const [presentStarterPacksModal, onDismissStarterPacks] = useIonModal(
+    StarterPacksModal,
+    {
+      onDismiss: () => onDismissStarterPacks(),
+    },
+  );
+
+  return (
+    <MaxWidthContainer>
+      <div className="ion-padding">
+        <p>
+          This is your{" "}
+          <IonText color="primary">
+            <strong>home feed</strong>
+          </IonText>
+          . It shows posts from communities you follow. But it&apos;s empty
+          right now!
+        </p>
+        <p>
+          <strong>Follow some communities to get started.</strong> Then,{" "}
+          <strong>pull to refresh.</strong> Or browse{" "}
+          <IonText color="primary">
+            <strong>all posts</strong>
+          </IonText>{" "}
+          to see everything.
+        </p>
+      </div>
+      <IonList inset>
+        <IonItem
+          onClick={() =>
+            router.push(buildGeneralBrowseLink("/all"), "none", "replace")
+          }
+          button
+        >
+          <IonIcon icon={earthOutline} slot="start" color="primary" /> All Posts
+        </IonItem>
+        <IonItem
+          button
+          onClick={() =>
+            presentStarterPacksModal({
+              presentingElement:
+                pageRef?.current?.closest("ion-tabs") ?? undefined,
+            })
+          }
+        >
+          <IonIcon icon={duplicateOutline} slot="start" color="primary" />
+          Starter Community Packs
+        </IonItem>
+      </IonList>
+    </MaxWidthContainer>
+  );
+}
