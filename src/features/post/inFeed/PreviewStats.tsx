a7188b61451768c2c937a1d2e5e3045164b35550
jmac <jason.mac.laptop@gmail.com>
2025-04-27 01:27:00 -0700

    Fix PreviewStats alignment for large text.

--- src/features/post/inFeed/PreviewStats.tsx
+++ src/features/post/inFeed/PreviewStats.tsx
@@ -8,6 +8,8 @@ import TimeStat from "#/features/post/detail/TimeStat";
 import { getCounts } from "#/helpers/lemmyCompat";
 import { formatNumber } from "#/helpers/number";
 
+import { useAppSelector } from "#/store";
+
 import styles from "./PreviewStats.module.css";
 
 interface PreviewStatsProps {
@@ -15,6 +17,30 @@ interface PreviewStatsProps {
 }
 
 export default function PreviewStats({ post }: PreviewStatsProps) {
+
+  const accommodateLargeText = useAppSelector(
+    (state) => state.settings.appearance.font.accommodateLargeText,
+  );
+
+  if (accommodateLargeText) {
+    return (
+      <>
+        <div className={styles.containerAlignLeft}>
+          <Vote item={post} />
+        </div>
+        <div className={styles.containerAlignLeft}>
+          <Stat icon={chatbubbleOutline}>
+            {formatNumber(getCounts(post).comments)}
+          </Stat>
+          <div className={styles.spacer} />
+          <TimeStat>
+            <Ago date={post.post.published} />
+          </TimeStat>
+        </div>
+      </>
+    );
+  }
+
   return (
     <div className={styles.container}>
       <Vote item={post} />
