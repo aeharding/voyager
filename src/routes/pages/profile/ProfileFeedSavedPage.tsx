663adc6f259603987c7ff910899ccf12e3355db1
Alexander Harding <noreply@harding.dev>
2025-06-24 22:44:11 -0500

    refactor: zod runtime type checks (#2055)

--- src/routes/pages/profile/ProfileFeedSavedPage.tsx
+++ src/routes/pages/profile/ProfileFeedSavedPage.tsx
@@ -2,20 +2,25 @@ import { useParams } from "react-router-dom";
 
 import { FetchFn } from "#/features/feed/Feed";
 import { PostCommentItem } from "#/features/feed/PostCommentFeed";
+import { getUserIfNeeded } from "#/features/user/userSlice";
 import useClient from "#/helpers/useClient";
 import { LIMIT } from "#/services/lemmy";
+import { useAppDispatch } from "#/store";
 
 import BaseProfileFeedItemsPage from "./BaseProfileFeedItemsPage";
 
 export default function ProfileFeedSavedPage() {
   const { handle } = useParams<{ handle: string }>();
   const client = useClient();
+  const dispatch = useAppDispatch();
 
   const fetchFn: FetchFn<PostCommentItem> = async (pageData, ...rest) => {
+    const person = await dispatch(getUserIfNeeded(handle));
+
     const { content } = await client.listPersonSaved(
       {
         ...pageData,
-        username: handle,
+        person_id: person.id,
         limit: LIMIT,
       },
       ...rest,
