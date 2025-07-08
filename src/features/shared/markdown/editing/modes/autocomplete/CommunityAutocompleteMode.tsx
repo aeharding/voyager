6fc1e7d147cc83cda5086da4cf84885bdde19e25
Alexander Harding <noreply@harding.dev>
2025-07-08 18:56:55 -0500

    feat: autolink and autocomplete /u/user and /c/community links

--- src/features/shared/markdown/editing/modes/autocomplete/CommunityAutocompleteMode.tsx
+++ src/features/shared/markdown/editing/modes/autocomplete/CommunityAutocompleteMode.tsx
@@ -11,7 +11,7 @@ import GenericAutocompleteMode, {
 export default function CommunityAutocomplete(props: AutocompleteModeProps) {
   const client = useClient();
 
-  const fetchFn = async (q: string) => {
+  async function fetchFn(q: string) {
     const { data } = await client.search({
       q,
       type_: "Communities",
@@ -19,13 +19,13 @@ export default function CommunityAutocomplete(props: AutocompleteModeProps) {
     });
 
     return (data as CommunityView[]).map((view) => view.community);
-  };
+  }
+
+  function buildMd(item: Community) {
+    return `[${props.prefix}${getRemoteHandle(item)}](${item.actor_id})`;
+  }
 
   return (
     <GenericAutocompleteMode buildMd={buildMd} fetchFn={fetchFn} {...props} />
   );
-}
-
-function buildMd(item: Community) {
-  return `[!${getRemoteHandle(item)}](${item.actor_id})`;
 }
