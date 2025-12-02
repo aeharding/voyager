71d711ba37404c66019ac29090efdecd08ef07b1
Alexander Harding <noreply@harding.dev>
2025-12-01 19:41:27 -0600

    feat: unwrap fedi sharing services on paste in post url textbox

--- src/helpers/clipboard.ts
+++ src/helpers/clipboard.ts
@@ -1,13 +1,47 @@
-import { ClipboardEvent } from "react";
+import { ClipboardEvent, KeyboardEvent, useRef } from "react";
 
 import { extractLemmyLinkFromPotentialFediRedirectService } from "#/features/share/fediRedirect";
 import { GO_VOYAGER_HOST } from "#/features/share/fediRedirect";
 import { htmlToMarkdown } from "#/helpers/markdown";
 import { parseUriList } from "#/helpers/url";
 
-export function attemptParseFromClipboard(e: ClipboardEvent) {
+type TargetType = "markdown" | "url";
+
+export function useOnPaste(targetType: TargetType) {
+  const onPastePlainRef = useRef(false);
+
+  function onKeyUpDown(e: KeyboardEvent) {
+    // It's not enough to just check for shiftKey,
+    // because iOS safari will return shiftKey=true when
+    // <enter> or <space> is pressed (including on keyup).
+    onPastePlainRef.current = e.shiftKey && (e.metaKey || e.ctrlKey);
+  }
+
+  function onPaste(e: ClipboardEvent) {
+    // Bail on paste modifiers if user is holding down shift
+    if (onPastePlainRef.current) return;
+
+    const toInsert = attemptParseFromClipboard(e, targetType);
+    if (!toInsert) return;
+
+    e.preventDefault();
+    document.execCommand("insertText", false, toInsert);
+  }
+
+  return {
+    onKeyUpDown,
+    onPaste,
+  };
+}
+
+export function attemptParseFromClipboard(
+  e: ClipboardEvent,
+  targetType: TargetType,
+) {
   return (
-    attemptParseHtmlFromClipboard(e) ||
+    (targetType === "markdown"
+      ? attemptParseHtmlFromClipboard(e)
+      : undefined) ||
     attemptParseUrlFromClipboard(e) ||
     attemptParseTextFromClipboard(e)
   );
