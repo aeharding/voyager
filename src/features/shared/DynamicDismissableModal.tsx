ff368992c610a0b5d61ff4fc989e34302214da11
Alexander Harding <noreply@harding.dev>
2025-12-09 22:23:20 -0600

    fix: attempt to fix android modal dismiss zoom (#2183)
    follow-up on a892591e44f121f911edb16a70f868f79f13fcb8.

--- src/features/shared/DynamicDismissableModal.tsx
+++ src/features/shared/DynamicDismissableModal.tsx
@@ -151,8 +151,13 @@ export function DynamicDismissableModal({
         canDismiss={canDismiss ? canDismiss : onDismissAttemptCb}
         onDidDismiss={() => {
           setIsOpen(false);
-          setPresentingElement(undefined);
 
+          // Attempt to fix https://github.com/aeharding/voyager/issues/2183
+          requestAnimationFrame(() => {
+            requestAnimationFrame(() => {
+              setPresentingElement(undefined);
+            });
+          });
           // in case onDidDismiss incorrectly called by Ionic, don't clear data
           if (textRecovery && canDismissRef_.current) clearRecoveredText();
 
