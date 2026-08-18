27c074a9a94157833bbcf3e56ce355e15dbbda0f
Alexander Harding <noreply@harding.dev>
2026-08-17 22:16:14 -0500

    fix: show two column setting in desktop app (#2346)

--- src/routes/twoColumn/useIsDeviceTwoColumnCapable.ts
+++ src/routes/twoColumn/useIsDeviceTwoColumnCapable.ts
@@ -1,5 +1,9 @@
 import { isPlatform } from "@ionic/core";
 
+import { getPlatform } from "#/helpers/device";
+
 export default function useIsDeviceTwoColumnCapable() {
+  if (getPlatform() === "tauri") return true;
+
   return isPlatform("desktop") || isPlatform("tablet") || isPlatform("phablet");
 }
